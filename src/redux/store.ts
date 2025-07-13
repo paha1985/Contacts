4b52a73edd4964b56b0b8c00184ca7868b0f6ab8
paha1985 <paha290585@gmail.com>
2025-07-14 01:27:37 +0800

    chore: replace createStore with configureStore

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,6 +3,7 @@ import contactsReducer from "./reducers/contacts-reducer";
 import gpoupsReducer from "./reducers/groups-reducer";
 import { favoritesReducer } from "./reducers/favorite-reducer";
 import {thunk} from 'redux-thunk';
+import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
@@ -11,8 +12,13 @@ const rootReducer = combineReducers({
 });
 
 // @ts-ignore
-export const store = createStore(
-  rootReducer, applyMiddleware(thunk)
-);
+// export const store = createStore(
+//   rootReducer, applyMiddleware(thunk)
+// );
+
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+})
 
 export type RootState = ReturnType<typeof rootReducer>;
